8ff74e512fd02ef881a377b8e7deaf5dadf5b23a
Feyzarkt <60755254+Feyzarkt@users.noreply.github.com>
2022-06-12 16:46:54 +0300

    add new screens

---
+++ app/src/main/java/com/feyzaurkut/movieapp/ui/favorites/FavoritesFragment.kt
@@ -0,0 +1,20 @@
+package com.feyzaurkut.movieapp.ui.favorites
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.feyzaurkut.movieapp.R
+
+
+class FavoritesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favorites, container, false)
+    }
+}
