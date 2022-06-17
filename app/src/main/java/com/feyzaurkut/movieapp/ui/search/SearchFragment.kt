b73554240fa4b636ce974a750d7adb1e63f0759d
Feyzarkt <60755254+Feyzarkt@users.noreply.github.com>
2022-06-17 20:57:15 +0300

    initialize search process

--- app/src/main/java/com/feyzaurkut/movieapp/ui/search/SearchFragment.kt
+++ app/src/main/java/com/feyzaurkut/movieapp/ui/search/SearchFragment.kt
@@ -1,20 +1,91 @@
 package com.feyzaurkut.movieapp.ui.search
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.findNavController
 import com.feyzaurkut.movieapp.R
+import com.feyzaurkut.movieapp.data.model.Movie
+import com.feyzaurkut.movieapp.data.model.RequestState
+import com.feyzaurkut.movieapp.databinding.FragmentSearchBinding
+import com.feyzaurkut.movieapp.ui.viewmodel.LocalViewModel
+import com.feyzaurkut.movieapp.ui.viewmodel.RemoteViewModel
+import com.feyzaurkut.movieapp.util.DataMapper
+import com.feyzaurkut.movieapp.util.OnClickListenerAdapter
+import com.feyzaurkut.movieapp.util.OnDoubleClickListenerAdapter
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class SearchFragment : Fragment() {
+
+    private lateinit var binding: FragmentSearchBinding
+    private val remoteViewModel: RemoteViewModel by viewModels()
+    private val localViewModel: LocalViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        binding = FragmentSearchBinding.inflate(inflater)
+        initListeners()
+        return binding.root
+    }
+
+    private fun initListeners() {
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (query != null) {
+                    remoteViewModel.searchMovie(query)
+                }
+                viewLifecycleOwner.lifecycleScope.launch {
+                    remoteViewModel.searchState.collect { requestState ->
+                        when (requestState) {
+                            is RequestState.Success -> {
+                                initRecycler(requestState.data.movies)
+                            }
+                        }
+                    }
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+    }
+
+    private fun initRecycler(list: ArrayList<Movie>) {
+        binding.rvSearchedMovies.apply {
+            adapter = SearchRecyclerAdapter(list, object : OnDoubleClickListenerAdapter {
+                override fun onClick(position: Int) {
+                    val favMovieInfo = DataMapper.mapMovieToMovieInfoEntities(list[position])
+                    localViewModel.insertFavMovie(favMovieInfo)
+                    Toast.makeText(
+                        requireContext(),
+                        "${favMovieInfo.title} added to favorites",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }, object : OnClickListenerAdapter {
+                override fun onClick(position: Int) {
+                    val bundle = bundleOf("movieInfo" to list[position])
+                    findNavController().navigate(
+                        R.id.action_searchFragment_to_detailFragment,
+                        bundle
+                    )
+                }
+            })
+        }
     }
 
 }
---
+++ app/src/main/java/com/feyzaurkut/movieapp/ui/search/SearchRecyclerAdapter.kt
@@ -0,0 +1,59 @@
+package com.feyzaurkut.movieapp.ui.search
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.feyzaurkut.movieapp.R
+import com.feyzaurkut.movieapp.data.model.Movie
+import com.feyzaurkut.movieapp.databinding.ItemMovieBinding
+import com.feyzaurkut.movieapp.util.*
+
+class SearchRecyclerAdapter (private val movieList: ArrayList<Movie>,
+                             private val onDoubleClickListenerAdapter: OnDoubleClickListenerAdapter,
+                             private val onClickListenerAdapter: OnClickListenerAdapter)
+    : ListAdapter<Movie, SearchRecyclerAdapter.ViewHolder>(DiffCallback()){
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        private val binding = ItemMovieBinding.bind(itemView)
+        fun bind(movie: Movie){
+            with(binding) {
+                movie.posterPath?.let { ivPoster.getPhoto(Constants.POSTER_URL+it) }
+                tvTitle.text = movie.title
+                cvMovie.setOnDoubleClickListener {
+                    onDoubleClickListenerAdapter.onClick(adapterPosition)
+                }
+                ivDetail.setOnClickListener {
+                    onClickListenerAdapter.onClick(adapterPosition)
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(movieList[position])
+
+    }
+
+    override fun getItemCount(): Int {
+        return movieList.size
+    }
+
+}
+
+class DiffCallback : DiffUtil.ItemCallback<Movie>() {
+    override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+        return oldItem == newItem
+    }
+}
