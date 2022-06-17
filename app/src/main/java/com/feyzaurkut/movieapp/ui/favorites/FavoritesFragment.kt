741a8ab11c9bb5e9ca38f63b204369b7ff6359b2
Feyzarkt <60755254+Feyzarkt@users.noreply.github.com>
2022-06-17 20:54:41 +0300

    init fav recycler

--- app/src/main/java/com/feyzaurkut/movieapp/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/feyzaurkut/movieapp/ui/favorites/FavoritesFragment.kt
@@ -1,20 +1,73 @@
 package com.feyzaurkut.movieapp.ui.favorites
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.findNavController
 import com.feyzaurkut.movieapp.R
+import com.feyzaurkut.movieapp.data.model.MovieInfoEntity
+import com.feyzaurkut.movieapp.data.model.RequestState
+import com.feyzaurkut.movieapp.databinding.FragmentFavoritesBinding
+import com.feyzaurkut.movieapp.ui.viewmodel.LocalViewModel
+import com.feyzaurkut.movieapp.util.DataMapper
+import com.feyzaurkut.movieapp.util.OnClickListenerAdapter
+import com.feyzaurkut.movieapp.util.RemoveAdapter
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-
+@AndroidEntryPoint
 class FavoritesFragment : Fragment() {
+
+    private lateinit var binding: FragmentFavoritesBinding
+    private val localViewModel: LocalViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorites, container, false)
+        binding = FragmentFavoritesBinding.inflate(inflater)
+
+        initObserver()
+
+        return binding.root
+    }
+
+    private fun initObserver() {
+        localViewModel.getFavMovies()
+        viewLifecycleOwner.lifecycleScope.launch {
+            localViewModel.favMoviesState.collect { requestState ->
+                when (requestState) {
+                    is RequestState.Success -> {
+                        initRecycler(requestState.data)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun initRecycler(list: List<MovieInfoEntity>) {
+        binding.rvFavMovies.apply {
+            adapter = FavoritesRecyclerAdapter(list, object : RemoveAdapter {
+                override fun onClick(position: Int) {
+                    list[position].id?.let { localViewModel.deleteFavMovie(it) }
+                    initObserver()
+                }
+            }, object : OnClickListenerAdapter {
+                override fun onClick(position: Int) {
+                    val movie = DataMapper.mapMovieInfoEntitiesToMovie(list[position])
+                    val bundle = bundleOf("movieInfo" to movie)
+                    findNavController().navigate(
+                        R.id.action_favoritesFragment_to_detailFragment,
+                        bundle
+                    )
+                }
+            })
+        }
     }
 }
---
+++ app/src/main/java/com/feyzaurkut/movieapp/ui/favorites/FavoritesRecyclerAdapter.kt
@@ -0,0 +1,64 @@
+package com.feyzaurkut.movieapp.ui.favorites
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.feyzaurkut.movieapp.R
+import com.feyzaurkut.movieapp.data.model.MovieInfoEntity
+import com.feyzaurkut.movieapp.databinding.ItemFavMovieBinding
+import com.feyzaurkut.movieapp.util.Constants
+import com.feyzaurkut.movieapp.util.OnClickListenerAdapter
+import com.feyzaurkut.movieapp.util.RemoveAdapter
+import com.feyzaurkut.movieapp.util.getPhoto
+
+class FavoritesRecyclerAdapter(
+    private val favMovieEntityList: List<MovieInfoEntity>,
+    private val removeAdapter: RemoveAdapter,
+    private val onClickListenerAdapter: OnClickListenerAdapter
+) : ListAdapter<MovieInfoEntity, FavoritesRecyclerAdapter.ViewHolder>(DiffCallback()) {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val binding = ItemFavMovieBinding.bind(itemView)
+        fun bind(movieEntity: MovieInfoEntity) {
+            with(binding) {
+                movieEntity.posterPath?.let { ivPoster.getPhoto(Constants.POSTER_URL + it) }
+                tvTitle.text = movieEntity.title
+                ivDelete.setOnClickListener {
+                    removeAdapter.onClick(adapterPosition)
+                }
+                ivDetail.setOnClickListener {
+                    onClickListenerAdapter.onClick(adapterPosition)
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_fav_movie, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(favMovieEntityList[position])
+
+    }
+
+    override fun getItemCount(): Int {
+        return favMovieEntityList.size
+    }
+
+}
+
+class DiffCallback : DiffUtil.ItemCallback<MovieInfoEntity>() {
+    override fun areItemsTheSame(oldItem: MovieInfoEntity, newItem: MovieInfoEntity): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: MovieInfoEntity, newItem: MovieInfoEntity): Boolean {
+        return oldItem == newItem
+    }
+}
