9a6af07f3240be408e550b6516a5d39b2ccf67c0
Feyzarkt <60755254+Feyzarkt@users.noreply.github.com>
2022-06-17 20:46:21 +0300

    update constants

--- app/src/main/java/com/feyzaurkut/movieapp/util/Constants.kt
+++ app/src/main/java/com/feyzaurkut/movieapp/util/Constants.kt
@@ -1,5 +1,6 @@
 package com.feyzaurkut.movieapp.util
 
 object Constants {
-    const val IMAGES_URL = "https://image.tmdb.org/t/p/w185_and_h278_bestv2"
+    const val POSTER_URL = "https://image.tmdb.org/t/p/w342"
+    const val BACKDROP_URL = "https://image.tmdb.org/t/p/w780"
 }
