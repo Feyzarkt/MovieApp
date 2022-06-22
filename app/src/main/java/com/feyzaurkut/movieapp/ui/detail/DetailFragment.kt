33769450ea911338f5aa76fa300d45ccc4700ae8
Feyzarkt <60755254+Feyzarkt@users.noreply.github.com>
2022-06-22 20:00:25 +0300

    check isFavorite & update ui

--- app/src/main/java/com/feyzaurkut/movieapp/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/feyzaurkut/movieapp/ui/detail/DetailFragment.kt
@@ -1,18 +1,22 @@
 package com.feyzaurkut.movieapp.ui.detail
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
-import com.feyzaurkut.movieapp.data.model.Genre
 import com.feyzaurkut.movieapp.data.model.Movie
 import com.feyzaurkut.movieapp.data.model.RequestState
 import com.feyzaurkut.movieapp.databinding.FragmentDetailBinding
+import com.feyzaurkut.movieapp.ui.viewmodel.LocalViewModel
 import com.feyzaurkut.movieapp.ui.viewmodel.RemoteViewModel
 import com.feyzaurkut.movieapp.util.Constants
+import com.feyzaurkut.movieapp.util.DataMapper
 import com.feyzaurkut.movieapp.util.getPhoto
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
@@ -24,6 +28,8 @@ class DetailFragment : Fragment() {
     private lateinit var binding: FragmentDetailBinding
     private val movieInfo: Movie by lazy { arguments?.getParcelable<Movie>("movieInfo") as Movie}
     private val remoteViewModel: RemoteViewModel by viewModels()
+    private val localViewModel: LocalViewModel by viewModels()
+    private var isFavorite: Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,6 +38,8 @@ class DetailFragment : Fragment() {
         binding = FragmentDetailBinding.inflate(inflater)
 
         getGenresFromApi()
+        initListener()
+        observeSearchedFavMovie()
 
         return binding.root
     }
@@ -42,16 +50,18 @@ class DetailFragment : Fragment() {
             ivBackdrop.getPhoto(Constants.BACKDROP_URL+movieInfo.backdropPath)
             ivPoster.getPhoto(Constants.POSTER_URL+movieInfo.posterPath)
             tvGenres.text = genres
-            tvReleaseDate.text = movieInfo.releaseDate
+            tvReleaseDate.text = "( " + movieInfo.releaseDate + " )"
             tvVoteAverage.text = movieInfo.voteAverage.toString()
             tvLanguage.text = movieInfo.originalLanguage?.uppercase()
-            tvPopularity.text = movieInfo.popularity.toString()
             tvOverview.text = movieInfo.overview
+
+            if (isFavorite) btnFav.setColorFilter(Color.RED)
+            else btnFav.setColorFilter(Color.WHITE)
         }
     }
 
     private fun getGenresFromApi() {
-        var genres = arrayListOf<String>()
+        val genres = arrayListOf<String>()
         remoteViewModel.getGenres()
         viewLifecycleOwner.lifecycleScope.launch {
             remoteViewModel.genresState.collect { requestState ->
@@ -71,4 +81,35 @@ class DetailFragment : Fragment() {
         }
     }
 
+    private fun initListener(){
+        binding.btnFav.setOnClickListener {
+            isFavorite = if (isFavorite) {
+                Log.e("if", isFavorite.toString())
+                movieInfo.id?.let { it1 -> localViewModel.deleteFavMovie(it1) }
+                binding.btnFav.setColorFilter(Color.WHITE)
+                false
+            } else {
+                Log.e("else", isFavorite.toString())
+                val favMovieInfo = DataMapper.mapMovieToMovieInfoEntities(movieInfo)
+                localViewModel.insertFavMovie(favMovieInfo)
+                binding.btnFav.setColorFilter(Color.RED)
+                true
+            }
+        }
+    }
+
+    private fun observeSearchedFavMovie(){
+        movieInfo.id?.let { localViewModel.searchFavMovie(it) }
+        viewLifecycleOwner.lifecycleScope.launch {
+            localViewModel.searchedMovieState.collect { requestState->
+                when(requestState) {
+                    is RequestState.Success -> {
+                        if(requestState.data != null) {
+                            isFavorite = true
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
