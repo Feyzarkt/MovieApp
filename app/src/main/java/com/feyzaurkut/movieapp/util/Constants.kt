e010e0b3d05478133334acd7d3860e886de0265c
Feyzarkt <60755254+Feyzarkt@users.noreply.github.com>
2022-07-07 08:47:28 +0300

    Add api key as a constant

--- app/src/main/java/com/feyzaurkut/movieapp/util/Constants.kt
+++ app/src/main/java/com/feyzaurkut/movieapp/util/Constants.kt
@@ -1,6 +1,7 @@
 package com.feyzaurkut.movieapp.util
 
 object Constants {
+    const val API_KEY = "3c7ab02722ce1ddde20129d8d03a8320"
     const val POSTER_URL = "https://image.tmdb.org/t/p/w342"
     const val BACKDROP_URL = "https://image.tmdb.org/t/p/w780"
 }
