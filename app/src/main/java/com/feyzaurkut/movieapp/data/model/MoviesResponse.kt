7f9a811bc0dfa0cdcb185151a367901c58a758a5
Feyzarkt <60755254+Feyzarkt@users.noreply.github.com>
2022-07-07 08:43:15 +0300

    Update movie models

--- app/src/main/java/com/feyzaurkut/movieapp/data/model/MoviesResponse.kt
+++ app/src/main/java/com/feyzaurkut/movieapp/data/model/MoviesResponse.kt
@@ -3,7 +3,6 @@ package com.feyzaurkut.movieapp.data.model
 import com.google.gson.annotations.SerializedName
 
 data class MoviesResponse(
-    @SerializedName("page")
     var page: Int? = null,
     @SerializedName("results")
     var movies: ArrayList<Movie> = arrayListOf()
